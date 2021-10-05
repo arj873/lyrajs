1361a4627f23b3d6cfa76fa8d926952c8621e565
Andy Jeong <andyjeong01@gmail.com>
2021-10-05 17:23:13 -0500

    minor changes to status.js

--- commands/utilities/status.js
+++ commands/utilities/status.js
@@ -1,5 +1,6 @@
 const { CommandInteraction, Client, MessageEmbed } = require("discord.js");
 const { connection } = require("mongoose");
+const ms = require('ms');
 require("../../events/client/ready");
 
 module.exports = {
@@ -15,9 +16,7 @@ module.exports = {
     const Response = new MessageEmbed()
       .setColor("BLURPLE")
       .setDescription(
-        `**Latency**: ${client.ws.ping} ms. **Uptime**: <t:${parseInt(
-          client.readyTimestamp / 1000
-        )}:R> **Database**: ${switchTo(connection.readyState)}`
+        `**Latency**: ${client.ws.ping} ms **Uptime**: ${ms(client.uptime, { long: true })} **Database**: ${switchTo(connection.readyState)}`
       );
 
     interaction.reply({ embeds: [Response] });
