b5474bae064962146aea01772020321a941076d1
Andy Jeong <andyjeong01@gmail.com>
2021-10-05 23:44:42 -0500

    got rid of ifs, added switch statements instead

--- commands/utilities/help.js
+++ commands/utilities/help.js
@@ -20,34 +20,34 @@ module.exports = {
     const { options } = interaction;
     const Response = new MessageEmbed().setColor("BLURPLE");
     const Target = options.getString("command");
+    switch(Target) {
+        case null:
+            Response.setColor("BLURPLE");
+            Response.setDescription(
+                "**List of Commands** - [clear] [status] [userinfo]"
+            );
+            return interaction.reply({ embeds: [Response], ephemeral: true });
+        
+        case "clear":
+            Response.setColor("BLURPLE");
+            Response.setDescription(
+                "**Clear** - Allows staff to delete messages from a certain user or a channel."
+            );
+            return interaction.reply({ embeds: [Response], ephemeral: true });
 
-    if (Target === null) {
-      Response.setColor("BLURPLE");
-      Response.setDescription(
-        "**List of Commands** - [clear] [status] [userinfo]"
-      );
-      return interaction.reply({ embeds: [Response], ephemeral: true });
+        case "status":
+            Response.setColor("BLURPLE");
+            Response.setDescription(
+                "**Status** - Displays a status of the bot, including the latency and the database."
+            );
+            return interaction.reply({ embeds: [Response], ephemeral: true });
+        
+        case "userinfo":
+            Response.setColor("BLURPLE");
+            Response.setDescription(
+                "**Userinfo**\n Right click on the user, navigate to Apps, and click on userinfo."
+            );
+            return interaction.reply({ embeds: [Response], ephemeral: true });
+        }; 
     }
-    if (Target === "clear") {
-      Response.setColor("BLURPLE");
-      Response.setDescription(
-        "**Clear** - Allows staff to delete messages from a certain user or a channel."
-      );
-      return interaction.reply({ embeds: [Response], ephemeral: true });
-    }
-    if (Target === "status") {
-      Response.setColor("BLURPLE");
-      Response.setDescription(
-        "**Status** - Displays a status of the bot, including the latency and the database."
-      );
-      return interaction.reply({ embeds: [Response], ephemeral: true });
-    }
-    if (Target === "userinfo") {
-      Response.setColor("BLURPLE");
-      Response.setDescription(
-        "**Userinfo**\n Right click on the user, navigate to Apps, and click on userinfo."
-      );
-      return interaction.reply({ embeds: [Response], ephemeral: true });
-    }
-  },
 };
